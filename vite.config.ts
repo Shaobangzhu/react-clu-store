6e23d1cd5b516a785f3cb2f728b31607f4757d70
Chaoran Lu <andyluloveusc@gmail.com>
2026-04-22 13:10:48 -0700

    chore:add alias to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const createAlias = (dirName: string) =>
+  path.resolve(__dirname, "src", dirName);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": createAlias(""), // src/. 项目根目录
+      "@components": createAlias("components"), // src/components/. 组件专用通道
+      "~img": createAlias("assets/img"), // src/assets/img/. 图片专用通道
+      "#types": createAlias("types"), // src/types/. 类型定义专用通道
+    },
   },
 });
